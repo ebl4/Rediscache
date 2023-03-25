6d24cbfd4a0ac3d54c080099f551e5926913e643
Edson <edbdelima@gmail.com>
2023-03-25 17:37:35 -0300

    Adicionar arquivos de projeto.

---
+++ Rediscache/Program.cs
@@ -0,0 +1,18 @@
+﻿using StackExchange.Redis;
+
+string connectionString = "CONNECTION_STRING";
+
+using (var cache = ConnectionMultiplexer.Connect(connectionString))
+{
+    IDatabase db = cache.GetDatabase();
+
+    var result = await db.ExecuteAsync("ping");
+    Console.WriteLine($"PING = {result.Type} : {result}");
+
+    bool setValue = await db.StringSetAsync("test:key", "100");
+    Console.WriteLine($"SET: {setValue}");
+
+    string getValue = await db.StringGetAsync("test:key");
+    Console.WriteLine($"GET: {getValue}");
+    Console.ReadKey();
+}
